f52076fc5df06205607f42889bed55952f1cd22d
ImTheRealIndianGuy <jitin.guntupalli@gmail.com>
2025-02-18 14:08:05 -0500

    Player and Powerups

--- Entities/Player/P_Scenes/_fake_gun_.gd
+++ Entities/Player/P_Scenes/_fake_gun_.gd
@@ -1,13 +1,4 @@
 extends Node2D
 
-@onready var main = get.tree().get_root().get_node("Stage")
-@onready var projectile = load("res://big_bullet.tcsn")
-func _ready():
-	shoot()
 
-func shoot():
-	var instance = big_bullet.instantiate()
-	instance.dir = rotation
-	instance.spawnPos = global_position
-	instance.spawnRot = rotation
-	main.add_child.call_deferred(instance)
+onready var bullet_scene = load("res://Entities/big_bullet.tcsn")
---
+++ Entities/big_bullet.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var dir = Vector2(1,0)
+export var bullet_speed = 1
+
+func _ready():
+    pass
+
+func _process(delta):
+    self.position += dir * delta * bullet_speed
+
+    if ($RayCast2D.is_colliding()):
+        var collid = $RayCast2D.get_collider().get_parent()
+        if (collid.type == "player"):
+            position += Vector2(2000,2000)
+
+
+func screen_exited():
+    get_parent().remove_child(self)
+    queue_free()

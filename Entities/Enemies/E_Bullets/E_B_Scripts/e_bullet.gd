7919af7401f3ebde6cf4bf01a5515a4b32abff53
S-J-K-F <Skey@ftstudent.org>
2025-02-20 14:06:02 -0500

    Bullets Kida workin? (deleted malware)

--- Entities/Enemies/E_Bullets/E_B_Scripts/e_bullet.gd
+++ Entities/Enemies/E_Bullets/E_B_Scripts/e_bullet.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-var bullet_scene = load("res://Entities/Enemies/E_Bullets/E_B_Scenes/e_bullet.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,10 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate(.05)
-
-	var b = bullet_scene.instance()
-	b.position = self.position
-	b.rotation = self.rotation
-	b.position.x += 100
-	add_child(b)
+	self.position.x += 1

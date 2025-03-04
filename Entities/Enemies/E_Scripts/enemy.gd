d4941b3c29bbeb0fa16b5a9cbbdf0862d6c109d9
S-J-K-F <Skey@ftstudent.org>
2025-03-04 14:05:40 -0500

    Bullet_is_different

--- Entities/Enemies/E_Scripts/enemy.gd
+++ Entities/Enemies/E_Scripts/enemy.gd
@@ -6,13 +6,13 @@ var bullet_scene = load("res://Entities/Enemies/E_Bullets/E_B_Scenes/e_bullet.ts
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Timer.set_wait_time(2)
+	$Timer.set_wait_time(0.005)
 	$Timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	rotate(0.05)
+	rotate(0.15)
 	
 
 func spawn_bullets():
@@ -26,12 +26,26 @@ func spawn_bullets():
 	#add_child(b2)
 	b2.position = self.position
 	b2.rotation = self.rotation
-	b2.dir = Vector2(2, 0)
+	b2.dir = Vector2(0, 1)
+
+	var b3 = bullet_scene.instantiate()
+	#add_child(b1)
+	b3.position = self.position
+	b3.rotation = self.rotation
+	b3.dir = Vector2(-1, 0)
+
+	var b4 = bullet_scene.instantiate()
+	#add_child(b2)
+	b4.position = self.position
+	b4.rotation = self.rotation
+	b4.dir = Vector2(0, -1)
 	
 	#b1.position.y += 35
 	#b2.position.y += 35
 	get_parent().add_child(b1)
 	get_parent().add_child(b2)
+	get_parent().add_child(b3)
+	get_parent().add_child(b4)
 
 ###timeout():
 func _on_timer_timeout():

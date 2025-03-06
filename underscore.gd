d276132defb1ddfb1c73d66187ad79d54aa94082
ImTheRealIndianGuy <jitin.guntupalli@gmail.com>
2025-03-06 14:03:13 -0500

    Very Good 1 pound fish, other than that, smart code.

--- underscore.gd
+++ underscore.gd
@@ -5,7 +5,6 @@ var JOY_AXIS_TRIGGER_RIGHT = KEY_DOWN
 var JOY_AXIS_TRIGGER_LEFT = 0 
 var JOY_AXIS_TRIGGER_RIGHT = 0
 func _input(event ; InputEvent):
-    if(event.is_action_pressed("JOY_AXIS_TRIGGER_LEFT")):
-        JOY_AXIS_TRIGGER_LEFT == 1
-        print_rich_change("A")
-
+	if(event.is_action_pressed("JOY_AXIS_TRIGGER_LEFT")):
+		JOY_AXIS_TRIGGER_LEFT == 1
+		print_rich_change("A")

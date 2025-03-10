0249d1076ea51e5c840a070b6e51299f844c5a7f
jfeatherston09 <jacksonleeolaf@gmail.com>
2025-03-10 13:49:23 -0400

    Update player.gd

--- Entities/Player/P_Scripts/player.gd
+++ Entities/Player/P_Scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
-@onready var sprite : Sprite2D = $Sprite2D
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 @export var playerId : int = 0
 
 func _physics_process(_delta):
@@ -14,7 +14,10 @@ func _physics_process(_delta):
 	var hDirection = Input.get_axis("p%s_left" % playerId, "p%s_right" % playerId)
 	if hDirection:
 		velocity.x = hDirection * SPEED
-
+		if hDirection < 0:
+			sprite.play("left")
+		else:
+			sprite.play("right")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
@@ -22,6 +25,10 @@ func _physics_process(_delta):
 	var vDirection = Input.get_axis("p%s_up" % playerId, "p%s_down" % playerId)
 	if vDirection:
 		velocity.y = vDirection * SPEED
+		if vDirection < 0:
+			sprite.play("up")
+		else:
+			sprite.play("down")
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 

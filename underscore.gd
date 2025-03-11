76ec4a92d4797c69566221d0595e31fe60607503
ImTheRealIndianGuy <jitin.guntupalli@gmail.com>
2025-03-11 13:50:06 -0400

    Great undeerstanding and progress on the "Please Enter Your Name""

--- underscore.gd
+++ underscore.gd
@@ -1,10 +1,16 @@
 extends RichTextLabel
-
 var JOY_AXIS_TRIGGER_LEFT = KEY_UP
 var JOY_AXIS_TRIGGER_RIGHT = KEY_DOWN
-var JOY_AXIS_TRIGGER_LEFT = 0 
-var JOY_AXIS_TRIGGER_RIGHT = 0
-func _input(event ; InputEvent):
-	if(event.is_action_pressed("JOY_AXIS_TRIGGER_LEFT")):
-		JOY_AXIS_TRIGGER_LEFT == 1
-		print_rich_change("A")
+func _input(event:InputEvent):
+    if(event.is_action_pressed("JOY_AXIS_TRIGGER_LEFT")):
+        ("2370,56")
+        var JOY_AXIS_TRIGGER_LEFT=+1 
+    
+    else: 
+
+        var JOY_AXIS_TRIGGER_LEFT = 0 
+    if(event.is_action_pressed("JOY_AXIS_TRIGGER_LEFT")):
+
+     pass 
+            
+            

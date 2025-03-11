1fd2ba77b16e532ff93e8733f99cae7dc341cbb3
zoomtroopo <FranklinFranklin275@gmail.com>
2025-03-11 14:08:30 -0400

    start working on the bar

--- Entities/Enemies/E_Scripts/enemy.gd
+++ Entities/Enemies/E_Scripts/enemy.gd
@@ -1,33 +1,20 @@
 extends Node2D
 
-class_name Enemy
-
 var bullet_scene = load("res://Entities/Enemies/E_Bullets/E_B_Scenes/e_bullet.tscn")
 
-@onready var player = get_parent().get_node("Player")
-
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	$Timer.set_wait_time(0.6)
-	$Timer.start()
+func _ready() -> void:
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	rotate(1 * delta)
+func _process(_delta):
+	rotate(.05)
 	
+	var b = bullet_scene.instantiate()
+	add_child(b)
+	b.position = self.position
+	b.rotation = self.rotation
 
-func spawn_bullets():
-	var b1 = bullet_scene.instantiate()
-	#add_child(b1)
-	b1.position = self.position
-	b1.dir = Vector2(player.position.x - self.position.x, player.position.y - self.position.y).normalized()
-	
-	#b1.position.y += 35
-	#b2.position.y += 35
-	get_parent().add_child(b1)
-
-
-###timeout():
-func _on_timer_timeout():
-	spawn_bullets()
+	b.position.y += 35
+	get_parent().add_child(b)

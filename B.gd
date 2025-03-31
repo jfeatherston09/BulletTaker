990a53f5b61ddc59317eaa35ff5b8aae91662828
ImTheRealIndianGuy <jitin.guntupalli@gmail.com>
2025-03-31 14:06:37 -0400

    Did a few more letters on the "Please enter your name" which includes, A,B,C,D,and E. "

---
+++ B.gd
@@ -0,0 +1,6 @@
+extends RichTextLabel
+
+
+if(event.is_action_pressed("JOY_AXIS_TRIGGER_LEFT")) and var JOY_AXIS_TRIGGER_LEFT == 1:
+	(789,243)
+
--- underscore.gd
+++ underscore.gd
@@ -1,4 +1,6 @@
 extends RichTextLabel
+JOY_AXIS_TRIGGER_LEFT = KEY_UP
+JOY_AXIS_TRIGGER_RIGHT = KEY_DOWN
 var JOY_AXIS_TRIGGER_LEFT = KEY_UP
 var JOY_AXIS_TRIGGER_RIGHT = KEY_DOWN
 func _input(event:InputEvent):
@@ -9,4 +11,10 @@ func _input(event:InputEvent):
     else: 
 
         var JOY_AXIS_TRIGGER_LEFT = 0 
-    
+    if(event.is_action_pressed("JOY_AXIS_TRIGGER_LEFT")) and var JOY_AXIS_TRIGGER_LEFT = 1:
+
+            ("2370,56")
+            var JOY_AXIS_TRIGGER_LEFT=+1 
+    else: 
+
+        var JOY_AXIS_TRIGGER_LEFT = 1

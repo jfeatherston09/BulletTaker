d34d05a88c3e256a212224c29713ec8be69887e8
ImTheRealIndianGuy <jitin.guntupalli@gmail.com>
2025-04-01 14:07:59 -0400

    Need to Joystick to do further testing

--- underscore.gd
+++ underscore.gd
@@ -4,17 +4,17 @@ JOY_AXIS_TRIGGER_RIGHT = KEY_DOWN
 var JOY_AXIS_TRIGGER_LEFT = KEY_UP
 var JOY_AXIS_TRIGGER_RIGHT = KEY_DOWN
 func _input(event:InputEvent):
-    if(event.is_action_pressed("JOY_AXIS_TRIGGER_LEFT")):
-        ("2370,56")
-        var JOY_AXIS_TRIGGER_LEFT=+1 
-    
-    else: 
+	if(event.is_action_pressed("JOY_AXIS_TRIGGER_LEFT")):
+		("2370,56")
+		var JOY_AXIS_TRIGGER_LEFT=+1 
+	
+	else: 
 
-        var JOY_AXIS_TRIGGER_LEFT = 0 
-    if(event.is_action_pressed("JOY_AXIS_TRIGGER_LEFT")) and var JOY_AXIS_TRIGGER_LEFT = 1:
+		var JOY_AXIS_TRIGGER_LEFT = 0 
+	if(event.is_action_pressed("JOY_AXIS_TRIGGER_LEFT")) and var JOY_AXIS_TRIGGER_LEFT = 1:
 
-            ("2370,56")
-            var JOY_AXIS_TRIGGER_LEFT=+1 
-    else: 
+			("2370,56")
+			var JOY_AXIS_TRIGGER_LEFT=+1 
+	else: 
 
-        var JOY_AXIS_TRIGGER_LEFT = 1
+		var JOY_AXIS_TRIGGER_LEFT = 1

4bc8721589269e7fae1eeed301870514346ad493
S-J-K-F <Skey@ftstudent.org>
2025-04-03 13:36:16 -0400

    Camera fix

--- Entities/Player/P_Scripts/player.gd
+++ Entities/Player/P_Scripts/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-@export var speed = 50
+@export var speed = 100
 
 func get_input():
 	var input_direction = Input.get_vector("p1_left", "p1_right", "p1_up", "p1_down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()

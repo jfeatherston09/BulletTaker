72e623f047b2cc37109e9b216fd1ed3f2c2febce
S-J-K-F <Skey@ftstudent.org>
2025-04-10 13:27:41 -0400

    Enemy_Fixeses

--- Entities/Enemies/E_Scripts/enemy_spawn.gd
+++ Entities/Enemies/E_Scripts/enemy_spawn.gd
@@ -7,13 +7,13 @@ var enemy_scene = [load("res://Entities/Enemies/E_Scenes/enemy.tscn")
 ]
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Timer.set_wait_time(0.6)
+	$Timer.set_wait_time(10)
 	$Timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_timer_timeout():
-	for i in range(10):
+	for i in range(5):
 		var enemy = enemy_scene[0].instantiate()
 		randomize()
 		enemy.position.x = randf_range(-190, 190)

cc57797ff4acb8624da9d338f07e494827fd84b6
S-J-K-F <Skey@ftstudent.org>
2025-04-14 14:07:12 -0400

    Another one

--- Entities/Enemies/E_Scripts/enemy_spawn_2.gd
+++ Entities/Enemies/E_Scripts/enemy_spawn_2.gd
@@ -15,19 +15,18 @@
 #		enemy.position.x = randf_range(-190, 190)
 #		enemy.position.y = -107
 #		add_child(enemy)
-extends Node  # or `Spawner` if Spawner is a custom class
-
-# Declare the enemy scene array
-var enemy_scene = [load("res://Entities/Enemies/E_Scenes/enemy_2.tscn")]
+extends Spawner
 
 func _ready():
-    $Timer.set_wait_time(20)  # Set the wait time for enemy spawn
+    enemy_scene = [load("res://Entities/Enemies/E_Scenes/enemy_2.tscn")]
+    randomize()
+    $Timer.set_wait_time(20)
     $Timer.start()
 
 func _on_timer_timeout():
     for i in range(2):  # Spawn 2 enemies per timer timeout
-        var enemy = enemy_scene[0].instantiate()  # Instantiate an enemy from the array
-        randomize()  # Ensures randomness
-        enemy.position.x = randf_range(-190, 190)  # Random X position
-        enemy.position.y = -107  # Set Y position (off-screen to start)
-        add_child(enemy)  # Add the enemy to the scene
+        var enemy = enemy_scene[0].instantiate()
+        enemy.position.x = randf_range(-190, 190)
+        enemy.position.y = -107
+        add_child(enemy)
+

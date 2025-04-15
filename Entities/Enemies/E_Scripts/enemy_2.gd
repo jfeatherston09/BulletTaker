06b9c1103351672f5817ca144935647d4a4b6114
S-J-K-F <Skey@ftstudent.org>
2025-04-15 13:31:18 -0400

    Slower instead

--- Entities/Enemies/E_Scripts/enemy_2.gd
+++ Entities/Enemies/E_Scripts/enemy_2.gd
@@ -2,8 +2,8 @@
 extends Enemy
 
 func _process(delta):
-	# Override movement to go faster
-	position.y += 120 * delta
+	# Override movement to go slower
+	position.y += 10 * delta
 
 	# Keep original behavior (if needed)
 	if position.y > get_viewport_rect().size.y + 20:

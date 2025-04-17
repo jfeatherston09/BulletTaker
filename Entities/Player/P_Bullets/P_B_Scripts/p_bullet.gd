60cf2f520e5ce1bc25a14ca91e5a095660d045f2
jfeatherston09 <jacksonleeolaf@gmail.com>
2025-04-17 13:40:38 -0400

    Create p_bullet.gd

---
+++ Entities/Player/P_Bullets/P_B_Scripts/p_bullet.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+class_name Bullet
+
+var dir = Vector2(0, 1)
+
+@export var bullet_speed = 1
+@export var final_speed = 100
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.position -= dir * delta * bullet_speed
+
+	if ($RayCast2D.is_colliding()):
+		var collid = $RayCast2D.get_collider().get_parent()
+		if (collid.type == "PLAYER"):
+			position += Vector2(2000, 2000)
+
+			if (collid.maxHealth > 0):
+				collid.maxHealth -= 1
+			collid.advance_status()
+
+
+
+func screen_exited():
+	get_parent().remove_child(self)
+	queue_free()

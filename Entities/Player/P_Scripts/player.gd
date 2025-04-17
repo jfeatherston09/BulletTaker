67808d87f4c881382a0434e89d722c8982f0ae89
jfeatherston09 <jacksonleeolaf@gmail.com>
2025-04-17 13:09:33 -0400

    Update player.gd

--- Entities/Player/P_Scripts/player.gd
+++ Entities/Player/P_Scripts/player.gd
@@ -8,7 +8,7 @@ const SPEED = 100.0
 @onready var sprite : Sprite2D = $Sprite2D
 @export var playerId : int = 0
 
-@export var maxHealth = 200
+@export var maxHealth = 3
 @onready var currentHealth: int = maxHealth
 
 @onready var score_display = get_node("/root/Main/guiV2/panelContainer/score")
@@ -16,7 +16,7 @@ const SPEED = 100.0
 func _on_hit_enemy():
 	score_display.add_score(10)
 	sprite.play("hit_enemy")
-	
+
 func _on_got_hit():
 	score_display.subtrct_score(5)
 	sprite.play("healthSequences")
@@ -45,3 +45,6 @@ func _physics_process(_delta):
 func get_input():
 	var input_direction = Input.get_vector("p1_left", "p1_right", "p1_up", "p1_down")
 	velocity = input_direction * speed
+
+func advance_status():
+	get_parent().get_node("Label").text = get_parent().health_status[maxHealth]

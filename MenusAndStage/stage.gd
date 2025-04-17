9d65ae1074ae24d26d0ae0c13010247705771c90
jfeatherston09 <jacksonleeolaf@gmail.com>
2025-04-17 13:09:35 -0400

    Create stage.gd

---
+++ MenusAndStage/stage.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+var health_status = [
+	"REST IN PEPPERONI BOZO",
+	"YEOWCH YOU LOST A LIFE",
+	"YOUR SHIP IS AT FULL HEALTH"
+]
+
+func _ready() -> void:Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)

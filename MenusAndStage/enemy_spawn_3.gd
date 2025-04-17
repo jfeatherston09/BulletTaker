070ef102127ba212e65d8c618bc6e93ea10e13b6
S-J-K-F <Skey@ftstudent.org>
2025-04-17 13:15:04 -0400

    Final enemy

--- MenusAndStage/enemy_spawn_3.gd
+++ MenusAndStage/enemy_spawn_3.gd
@@ -3,11 +3,11 @@ extends Spawner
 func _ready():
     enemy_scene = [load("res://Entities/Enemies/E_Scenes/enemy_3.tscn")]
     randomize()
-    $Timer.set_wait_time(25)
+    $Timer.set_wait_time(60)
     $Timer.start()
 
 func _on_timer_timeout():
-    for i in range(4):  # Spawn 2 enemies per timer timeout
+    for i in range(1):  # Spawn 2 enemies per timer timeout
         var enemy = enemy_scene[0].instantiate()
         enemy.position.x = randf_range(-190, 190)
         enemy.position.y = -107

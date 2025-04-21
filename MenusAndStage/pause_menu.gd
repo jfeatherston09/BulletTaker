d4ed244bc35f494f2035720d9f49ed8f95cc5310
S-J-K-F <Skey@ftstudent.org>
2025-04-21 14:08:17 -0400

    Jumping Ai next time I meet them

--- MenusAndStage/pause_menu.gd
+++ MenusAndStage/pause_menu.gd
@@ -1,24 +1,8 @@
-extends Control
-
-@onready var main = $"../../"  # Or wherever your main scene node is
+extends CanvasLayer
 
 func _ready():
-    # Prevent this menu from pausing along with the game
-    process_mode = Node.PROCESS_MODE_ALWAYS
-    visible = false
+    pause_mode = "process"  # Keep processing when paused
 
-func open():
-    visible = true
-    get_tree().paused = true
-
-func close():
-    visible = false
-    get_tree().paused = false
-
-func _on_resume_pressed():
-    pause_menu.close()
-
-func _on_quit_pressed():
-    get_tree().quit()
-
-
+func set_paused(paused: bool):
+    get_tree().paused = paused
+    visible = paused
--- MenusAndStage/stage.gd
+++ MenusAndStage/stage.gd
@@ -1,18 +1,18 @@
-extends Node2D
+extends Node
 
-@onready var pause_menu = $CanvasLayer/PauseMenu
-@onready var resume_button = $CanvasLayer/PauseMenu/MarginContainer/VBoxContainer/Resume
-@onready var quit_button = $CanvasLayer/PauseMenu/MarginContainer/VBoxContainer/Quit
+@onready var pause_menu = $PauseMenu
 
-func _input(event):
-	if event.is_action_pressed("p1_start") or event.is_action_pressed("p2_start"):
-		if get_tree().paused:
-			resume_button.emit_signal("pressed")
-		else:
-			pause_menu.open()
+func _unhandled_input(event):
+    if Input.is_action_just_pressed("p1_start") or Input.is_action_just_pressed("p2_start"):
+        toggle_pause()
 
-	if (event.is_action_pressed("p1_x") or event.is_action_pressed("p2_x")) and get_tree().paused:
-		quit_button.emit_signal("pressed")
+    if get_tree().paused and (Input.is_action_just_pressed("p1_x") or Input.is_action_just_pressed("p2_x")):
+        get_tree().quit()
+
+func toggle_pause():
+    var currently_paused = get_tree().paused
+    pause_menu.set_paused(!currently_paused)
+
 
 
 

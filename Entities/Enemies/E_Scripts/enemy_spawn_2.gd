76184d06476eda833c96c4514ac2c641d4667ab9
S-J-K-F <Skey@ftstudent.org>
2025-04-22 14:06:40 -0400

    Quick fix

--- Entities/Enemies/E_Scripts/enemy_spawn_2.gd
+++ Entities/Enemies/E_Scripts/enemy_spawn_2.gd
@@ -1,15 +1,14 @@
 extends Spawner
 
 func _ready():
-    enemy_scene = [load("res://Entities/Enemies/E_Scenes/enemy_2.tscn")]
-    randomize()
-    $Timer.set_wait_time(20)
-    $Timer.start()
+	enemy_scene = [load("res://Entities/Enemies/E_Scenes/enemy_2.tscn")]
+	randomize()
+	$Timer.set_wait_time(2)
+	$Timer.start()
 
 func _on_timer_timeout():
-    for i in range(2):  # Spawn 2 enemies per timer timeout
-        var enemy = enemy_scene[0].instantiate()
-        enemy.position.x = randf_range(-190, 190)
-        enemy.position.y = -107
-        add_child(enemy)
-
+	for i in range(2):  # Spawn 2 enemies per timer timeout
+		var enemy = enemy_scene[0].instantiate()
+		enemy.position.x = randf_range(-190, 190)
+		enemy.position.y = -107
+		add_child(enemy)
--- MenusAndStage/enemy_spawn_3.gd
+++ MenusAndStage/enemy_spawn_3.gd
@@ -1,14 +1,14 @@
 extends Spawner
 
 func _ready():
-    enemy_scene = [load("res://Entities/Enemies/E_Scenes/enemy_3.tscn")]
-    randomize()
-    $Timer.set_wait_time(60)
-    $Timer.start()
+	enemy_scene = [load("res://Entities/Enemies/E_Scenes/enemy_3.tscn")]
+	randomize()
+	$Timer.set_wait_time(60)
+	$Timer.start()
 
 func _on_timer_timeout():
-    for i in range(1):  # Spawn 2 enemies per timer timeout
-        var enemy = enemy_scene[0].instantiate()
-        enemy.position.x = randf_range(-190, 190)
-        enemy.position.y = -107
-        add_child(enemy)
+	for i in range(1):  # Spawn 2 enemies per timer timeout
+		var enemy = enemy_scene[0].instantiate()
+		enemy.position.x = randf_range(-190, 190)
+		enemy.position.y = -107
+		add_child(enemy)

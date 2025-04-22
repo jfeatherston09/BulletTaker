0453bc5a10c4b43c63192dd5f0b095e3576ca82b
jfeatherston09 <jacksonleeolaf@gmail.com>
2025-04-22 14:08:09 -0400

    Update e_bullet.gd

--- Entities/Enemies/E_Bullets/E_B_Scripts/e_bullet.gd
+++ Entities/Enemies/E_Bullets/E_B_Scripts/e_bullet.gd
@@ -3,7 +3,8 @@ extends Node2D
 class_name Bullet
 
 var dir = Vector2(1, 0)
-var gui_scene = preload("res://MenusAndStage/guiV2.tscn").instantiate()
+@onready var gui_scene = preload("res://MenusAndStage/guiV2.tscn").instantiate()
+@onready var healthBar = gui_scene.get_node("VBoxContainer/health")
 
 @export var bullet_speed = 100
 @export var final_speed = 100
@@ -23,7 +24,7 @@ func _process(delta):
 
 			if (collid.maxHealth > 0):
 				collid.maxHealth -= 1
-				gui_scene.get_node("VBoxContainer/health").frame += 1
+				healthBar.frame += 1
 			collid.advance_status()
 
 

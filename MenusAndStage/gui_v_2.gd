d16d2d4f334d3e0073c6152ea8d70d459e00d6bd
jfeatherston09 <jacksonleeolaf@gmail.com>
2025-04-22 13:58:58 -0400

    Update gui_v_2.gd

--- MenusAndStage/gui_v_2.gd
+++ MenusAndStage/gui_v_2.gd
@@ -3,8 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimatedSprite2D.set_FRAME(1)
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:

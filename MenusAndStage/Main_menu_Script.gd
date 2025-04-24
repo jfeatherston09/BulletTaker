499e3ab1051c05618d91e2524fc486a386dbfc45
S-J-K-F <Skey@ftstudent.org>
2025-04-24 14:07:43 -0400

    MainMenuDOne:sunglasses emoji:

--- MenusAndStage/Main_menu_Script.gd
+++ MenusAndStage/Main_menu_Script.gd
@@ -1,23 +1,21 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("p1_start") or Input.is_action_just_pressed("p2_start"):
+		_on_start_pressed()
+	elif Input.is_action_just_pressed("p1_x") or Input.is_action_just_pressed("p2_x"):
+		_on_button_3_pressed()
+	elif Input.is_action_just_pressed("p1_b") or Input.is_action_just_pressed("p2_b"):
+		_on_quit_pressed()
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://MenusAndStage/Stage.tscn")
 
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_button_3_pressed() -> void:
-	get_tree().change_scene_to_file("res://control.tscn")
+	get_tree().change_scene_to_file("res://MenusAndStage/control.tscn")
---
+++ MenusAndStage/control.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("p1_b") or Input.is_action_just_pressed("p2_b"):
+		_on_start_pressed()
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://MenusAndStage/Main_Menu_Scene.tscn")

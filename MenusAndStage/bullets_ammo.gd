a78044774b6c3382a3119535c036e5bd89335dc5
jfeatherston09 <jacksonleeolaf@gmail.com>
2025-05-01 13:27:36 -0400

    Update bullets_ammo.gd

--- MenusAndStage/bullets_ammo.gd
+++ MenusAndStage/bullets_ammo.gd
@@ -1,11 +1,11 @@
 extends AnimatedSprite2D
-@onready var bulletGUI = $BulletsAmmo
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
-
+@onready var bulletGUI = $BulletsAmmo
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if (Input.is_action_just_pressed("Shoot")):

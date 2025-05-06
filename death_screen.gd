b04edbfda36289c05425fbab13f38427a753cabb
ImTheRealIndianGuy <jitin.guntupalli@gmail.com>
2025-05-06 13:56:46 -0400

    I am going to fix code

--- death_screen.gd
+++ death_screen.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 func _ready():
     if FrontalLobeScript.is_active == 2:
-        get_tree().change_scene_to_file("res://Underscore2.tscn")
+        var new_scene = load("res://Underscore2.tscn")
+        get_tree().change_scene_to_packed(new_scene)

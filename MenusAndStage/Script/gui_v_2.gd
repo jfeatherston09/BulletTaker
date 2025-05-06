1c377ebc29f065e93dc25307ad82fe516173ec0a
jfeatherston09 <jacksonleeolaf@gmail.com>
2025-05-06 13:53:56 -0400

    Update gui_v_2.gd

--- MenusAndStage/Script/gui_v_2.gd
+++ MenusAndStage/Script/gui_v_2.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var health_sprite = $VBoxContainer/Healthlost
+@onready var bulletBarrel = $BulletAmmo
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,7 +11,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if (Input.is_action_just_pressed("p1_b")):
+		bulletBarrel.frame =+ 1
+
 
 func update_Healthlost(new_health: int) -> void:
 	print("Health updated:", new_health)
@@ -19,3 +22,4 @@ func update_Healthlost(new_health: int) -> void:
 
 func _on_player_health_changed(new_health: int) -> void:
 	update_Healthlost(new_health)
+

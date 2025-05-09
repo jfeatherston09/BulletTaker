d7dff18cf0ae0865131f717aad650bec8d4de299
jfeatherston09 <jacksonleeolaf@gmail.com>
2025-05-09 14:00:03 -0400

    Update gui_v_2.gd

--- MenusAndStage/Script/gui_v_2.gd
+++ MenusAndStage/Script/gui_v_2.gd
@@ -7,16 +7,12 @@ func _ready() -> void:
 	var player = get_tree().root.get_node("Stage/Player")
 	player.health_changed.connect(_on_player_health_changed)
 @onready var bulletGUI = $BulletAmmo
-var currentFrame := 0
+var currentFrame = 0
 @onready var this_scene = $guiV2
-@onready var player_scene = load("res://Entities/Player/P_Scenes/player.tscn").instantiate()
-func bullet_data_transfer(old_scene, new_scene):
-	old_scene.currentFrame = new_scene.get_script().theFrame
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if (Input.is_action_just_pressed("p1_b")):
-		bullet_data_transfer(this_scene, player_scene)
 		if currentFrame < 16:
 			bulletGUI.frame += 1 
 			currentFrame += 1

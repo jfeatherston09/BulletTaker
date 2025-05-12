f3a2fd49d97d24a66a4263c9205c40f15b31cf38
S-J-K-F <Skey@ftstudent.org>
2025-05-12 13:36:50 -0400

    Sigma

--- game_over.gd
+++ game_over.gd
@@ -8,6 +8,8 @@ func _ready():
 	print("Loaded score on GameOver screen: ", GlobalState.final_score)
 
 	main_menu_button.pressed.connect(_on_main_menu_pressed)
-
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("p1_start") or Input.is_action_just_pressed("p2_start"):
+		_on_main_menu_pressed()
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://MenusAndStage/Scene/Main_Menu_Scene.tscn")

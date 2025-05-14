66c9eebc962408426e59477a2e4ca921ba4afe1d
zoomtroopo <FranklinFranklin275@gmail.com>
2025-05-14 14:10:48 -0400

    bugs
    Revert "bugs"
    
    This reverts commit 23c7302ea69d34b66570b5786b98762cb1f4492e.
    
    Revert "two player targeting"
    
    This reverts commit 0bf3406ea4b38de506ab905079455b9cf54b587c.

--- Entities/Enemies/E_Scripts/enemy.gd
+++ Entities/Enemies/E_Scripts/enemy.gd
@@ -3,11 +3,8 @@ extends Node2D
 class_name Enemy
 
 @onready var bullet_scene = load("res://Entities/Enemies/E_Bullets/E_B_Scenes/e_bullet.tscn")
-@onready var bullet_scene2 = load("res://Entities/Enemies/E_Bullets/E_B_Scenes/e_bullet_2.tscn")
 
 @onready var player = get_parent().get_parent().get_node("Player")
-
-@onready var player2 = get_parent().get_parent().get_node("Player2")
 
 var type = "ENEMY"
 @export var maxEnemyHealth: int = 3 - 1
@@ -38,15 +35,12 @@ func _process(delta):
 
 func spawn_bullets():
 	var b1 = bullet_scene.instantiate()
-	var b2 = bullet_scene2.instantiate()
+	
 	get_parent().add_child(b1)
 	b1.bullet_speed = 800	
 	b1.position = self.position
-	b1.dir = Vector2(player.global_position.x - self.global_position.x, player.global_position.y - self.global_position.y).normalized() #Vector2(player2.global_position.x - self.global_position.x, player2.global_position.y - self.global_position.y).normalized()
-	get_parent().add_child(b2)
-	b2.bullet_speed = 800	
-	b2.position = self.position
-	b2.dir2 = Vector2(player2.global_position.x - self.global_position.x, player2.global_position.y - self.global_position.y).normalized()
+	b1.dir = Vector2(player.global_position.x - self.global_position.x, player.global_position.y - self.global_position.y).normalized()
+
 func take_enemy_damge(_amount: int):
 	currentEnemyHealth -= _amount
 	if currentEnemyHealth == 0:
